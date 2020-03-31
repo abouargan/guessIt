be066766fe8c4f448c3f145d8b6ea3f616f46435
abouargan <bouargan@enset-media.ac.ma>
2020-03-31 19:45:20 +0100

    adding styles

--- main.php
+++ main.php
@@ -1,8 +1,11 @@
 <?php
-$rand = rand(1,10)*10;
+require 'index.html';
 $guess = $_GET['userNumber'];
-if($rand == $guess)
-    echo '<p>You\'e got it '. $rand .' is the correct number, well done !</p>';
-else
-    echo '<p>We\'re sorry to tell that your answer is wrong</p>';
+$rand = (rand(1,10)*10);
+$answer = ($rand == $guess);
+if($answer){
+    echo '<p style="text-align: center;font-size: 1.8em;color: green">You Win</p>';
+}else{
+    echo '<p style="text-align: center;font-size: 1.8em;color: red">You Loose</p>';
+}
 ?>
